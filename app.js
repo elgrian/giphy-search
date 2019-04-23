858585488428094d98c0b2465bc7444e06e84c37
elgrian <anwar.loves.lacroix@gmail.com>
2019-04-22 23:13:04 -0700

    Finished the tutorial and made the CSS a bit more neat and nice to look at.

--- app.js
+++ app.js
@@ -1,27 +1,61 @@
-// app.js
-// Require Libraries
-const express = require('express');
+var express = require('express');
+var app = express();
+var exphbs = require('express-handlebars')
+// REQUIRE HTTP MODULE
+var http = require('http');
+//INITIALIZE THE GIPHY-API LIBRARY
+// var giphy = require('giphy-api')();
 
-// App Setup
-const app = express();
-
-// Middleware
-const exphbs = require('express-handlebars');
-
+app.use(express.static('public'));
 app.engine('handlebars', exphbs({
     defaultLayout: 'main'
 }));
 app.set('view engine', 'handlebars');
 
-// Routes
-app.get('hello-gif', (req, res) => {
-    const gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
+app.get('/hello-gif', function (req, res) {
+    var gifUrl = 'http://media2.giphy.com/media/gYBVM1igrlzH2/giphy.gif'
     res.render('hello-gif', {
-        gifUrl
+        gifUrl: gifUrl
     })
-});
+})
 
-// Start Server
-app.listen(3000, () => {
-    console.log('Gif Search listening on port localhost:3000!');
+app.get('/greetings/:name', function (req, res) {
+    var name = req.params.name;
+    res.render('greetings', {
+        name: name
+    });
+})
+
+app.get('/', function (req, res) {
+    console.log(req.query.term)
+    var queryString = req.query.term;
+    // ENCODE THE QUERY STRING TO REMOVE WHITE SPACES AND RESTRICTED CHARACTERS
+    var term = encodeURIComponent(queryString);
+    // PUT THE SEARCH TERM INTO THE GIPHY API SEARCH URL
+    var url = 'http://api.giphy.com/v1/gifs/search?q=' + term + '&api_key=dc6zaTOxFJmzC'
+
+    http.get(url, function (response) {
+        // SET ENCODING OF RESPONSE TO UTF8
+        response.setEncoding('utf8');
+
+        var body = '';
+
+        response.on('data', function (d) {
+            // CONTINUOUSLY UPDATE STREAM WITH DATA FROM GIPHY
+            body += d;
+        });
+
+        response.on('end', function () {
+            // WHEN DATA IS FULLY RECEIVED PARSE INTO JSON
+            var parsed = JSON.parse(body);
+            // RENDER THE HOME TEMPLATE AND PASS THE GIF DATA IN TO THE TEMPLATE
+            res.render('home', {
+                gifs: parsed.data
+            })
+        });
+    });
+})
+
+app.listen(3000, function () {
+    console.log('Gif Search listening on port localhost:3000!')
 });
